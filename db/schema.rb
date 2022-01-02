7bfa7ad3cad83d39655cda069438a0adccd028c5
smualimc <smualim@hotmail.com>
2022-01-02 10:26:36 +0100

    Scaffold for Product and migrates db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_183135) do
+ActiveRecord::Schema.define(version: 2022_01_02_092015) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
