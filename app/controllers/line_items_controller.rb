e23849f8c7764400c276d5fc555d8662eb17ccf1
smualimc <smualim@hotmail.com>
2022-01-04 09:29:15 +0100

    edit line_items_controller,cart.rb & carts/show to count products & adds default value to quantity in line_items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,6 +24,7 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product)
     @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
