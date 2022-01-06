f5b009dc09e9afa5c7404dfa87d4a3b1a922e568
smualimc <smualim@hotmail.com>
2022-01-06 11:41:12 +0100

    Adds add_line_items_from_cart method in Order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,12 @@ class Order < ApplicationRecord
         "Tarjeta Crédito" => 1,
         "Orden de Compra" => 2
     }
+
+    def add_line_items_from_cart(cart)
+        cart.line_items.each do |item|
+            item.cart_id = nil
+            line_items << item
+        end
+    end
+
 end
