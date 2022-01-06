22d9be73c0f06a5d3c3f5f25745f63a333acd9e7
smualimc <smualim@hotmail.com>
2022-01-06 11:52:38 +0100

    Move pay_type validation below enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,12 @@
 class Order < ApplicationRecord
     has_many :line_items, dependent: :destroy 
     validates :name, :address, :email, presence: true
-    validates :pay_type, inclusion: pay_types.keys
     enum pay_type: {
         "Transferencia" => 0,
         "Tarjeta Crédito" => 1,
         "Orden de Compra" => 2
     }
+    validates :pay_type, inclusion: pay_types.keys
 
     def add_line_items_from_cart(cart)
         cart.line_items.each do |item|
