aa917b92d6b454126cf577b59ffc0d0fc4183a1b
smualimc <smualim@hotmail.com>
2022-01-06 10:48:00 +0100

    Fields validation for Order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+    validates :name, :address, :email, presence: true
+    validates :pay_type, inclusion: pay_types.keys
     enum pay_type: {
         "Transferencia" => 0,
         "Tarjeta Crédito" => 1,
