6eaa0facea247b0724b08a88559b1f00df8b9576
smualimc <smualim@hotmail.com>
2022-01-07 09:32:21 +0100

    Sale model define association populates uuid and validation

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,13 @@
 class Sale < ApplicationRecord
+    belongs_to :cart
+    before_create :populate_uuid
+
+    validates :total_price, numericality: { greater_than_or_equal_to: 0.50, message: "Valor no puede ser menor a 50 céntimos" }
+
+    private
+
+    def populate_uuid
+        self.uuid = SecureRandom.uuid()
+    end
 end
+
